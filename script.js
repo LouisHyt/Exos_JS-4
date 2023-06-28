c0b0b37b592fafe2c344d981513cf61baf781627
LouisHyt <louishayotte@gmail.com>
2023-06-28 15:25:30 +0200

    randomColor, Cache & keydown Event

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector(".container");
+let cache = [];
+
+window.addEventListener("keydown", (e) => {
+
+    if(e.key.toLowerCase() === "arrowup"){
+        if(cache.length === 0) return
+        cache[cache.length - 1].remove();
+        cache.pop();
+        return
+    }
+    let cube = document.createElement("div");
+    cube.classList.add("cube");
+    let randomColor = getRandomColor();
+    cube.style.backgroundColor = randomColor;
+    container.appendChild(cube);
+    cache.push(cube);
+})
+
+
+function getRandomColor() {
+    const characters = '0123456789ABCDEF'.split('');
+    let color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += characters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
